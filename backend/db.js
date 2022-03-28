5a069c4fa86d2d31fc547017646223ee9d2b044f
pavanshinde7494 <pavanshinde7494@gmail.com>
2022-03-28 14:45:25 +0530

    JWT started

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://localhost:27017/test?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
+const mongoURI = "mongodb://localhost:27017/iNotebook?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false";
 
 const connectToMongo = async()=>{
     await mongoose.connect(mongoURI , ()=>{
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,19 +2,64 @@ const express = require('express');
 const User = require('../models/User');
 
 const router = express.Router();
+const {body , validationResult} = require('express-validator');
 
+const bcrypt = require('bcrypt');
 
-
+const jwt = require('jsonwebtoken');
 
 
 // create a user using post . Doesn't require auth
 
-router.post('/' , (req,res)=>{
-    // console.log(req.body);
-    const user = User(req.body);
-    user.save();
-    console.log(req.body);
-    res.send('req.body')
+router.post('/createuser' ,[
+    body('name','Enter a Valid name').isLength({min : 3}),
+    body('email','Enter a valid Email').isEmail(),
+    body('password','Password must be at least 5 characters').isLength({ min: 5 }),
+]
+,async(req,res)=>{
+    // If there are errors return bad request
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    // Check whether user with this email exists
+    try{
+        let user = await User.findOne({
+            email : req.body.email
+        });
+        if(user){ 
+            return res.status(400).json({
+                error : "Sorry , This email already exists",
+                email : user.email
+            })
+        }
+        
+
+        const salt = await bcrypt.genSalt(10);
+        const secPass = await bcrypt.hash(req.body.password , salt);
+
+        // Creating a new user
+        user = await User.create({
+            name: req.body.name,
+            email : req.body.email,     
+            password: secPass
+        })
+
+        const data = {
+            user : user._id
+        }
+
+        const authToken = jwt.sign(data, 'secret');
+        res.json({authToken});
+    }
+    catch(err){
+        console.log(err)
+        res.status(500).json({
+            msg : err.message
+        });
+    }
+
 })
 
 
